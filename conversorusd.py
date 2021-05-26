91abe073b2b28242f9c89a388b31520cfb89410a
Andres Norato <carnorat@gmail.com>
2021-05-26 10:33:54 -0500

    Funciones en python

--- conversorusd.py
+++ conversorusd.py
@@ -1,8 +1,40 @@
-pesos = input("¿Cuántos pesos colombianos tienes: ")
-pesos = float(pesos)
-valor_dolar = 3740
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dólares")
+menu = """
+Bienvenido al conversor de monedas 💲
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos colombianos tienes: ")
+    pesos = float(pesos)
+    valor_dolar = 3740
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion ==2:
+    pesos = input("¿Cuántos pesos argentinos tienes: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+elif opcion ==3:
+    pesos = input("¿Cuántos pesos mexicanos tienes: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dólares")
+else:
+    print('Ingresa una opción valida por favor')
+
+
 
