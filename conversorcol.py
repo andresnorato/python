ab4efad35166387f170b244a8bce977f328db078
Andres Norato <carnorat@gmail.com>
2021-05-26 09:53:53 -0500

    Conversores de moneda en python

---
+++ conversorcol.py
@@ -0,0 +1,6 @@
+dolares = input("¿Cuantos dolares tienes?")
+dolares = int(dolares)
+valor_pesos_col = 3700
+dolares = valor_pesos_col * dolares
+dolares = str(dolares)
+print("Tienes $" + dolares + " pesos")
